413393c8b9eecc0008a1c46de96a7ae05ff3609e
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-04-01 11:59:58 -0400

    Downgraded tailwindcss to 3.3.5, imported photo assets, setup flowbite plugins, setup dynamic navbar and styled with flowbite

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    content: [
+        "./src/**/*.{html,ts}",
+        "./node_modules/flowbite/**/*.js"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                display: ['"Great Vibes"', ...defaultTheme.fontFamily.serif],
+                nav: ['"Marcellus"', ...defaultTheme.fontFamily.sans],
+                body: ['"Raleway"', ...defaultTheme.fontFamily.sans],
+            }
+        },
+    },
+    plugins: [
+        require('flowbite/plugin')
+    ],
+}
+
