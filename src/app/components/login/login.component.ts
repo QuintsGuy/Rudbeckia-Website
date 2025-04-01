0b96645f16d188cd81502084beaaf03b7f4179dc
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-04-01 17:07:43 -0400

    Design and styled login page, wired login component to supabase, guard, and auth.service.ts files

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,48 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  loginError: string | null = null;
+  showPassword: boolean = false;
+  loading: boolean = false;
 
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      remember: [false]
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    if (this.loginForm.invalid) return;
+    
+    const { email, password, remember } = this.loginForm.value;
+    console.log(email, password, remember);
+    this.loading = true;
+    this.loginError = await this.authService.login(email, password, remember);
+    this.loading = false;
+  }
+
+  async signInWithGoogle(): Promise<void> {
+    await this.authService.signInWithGoogle();
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 }
