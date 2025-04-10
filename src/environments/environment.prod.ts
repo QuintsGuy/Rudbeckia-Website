f71a773bf59e7519c4e2f41d98867d809b60bf1a
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-04-10 11:07:54 -0400

    Added environment file to git repo

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    SUPABASE_URL: process.env['SUPABASE_URL'] || '',
+    SUPABASE_KEY: process.env['SUPABASE_KEY'] || '',
+    bypassAuth: process.env['BYPASS_AUTH'] === 'false'
+};
