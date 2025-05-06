3884142088e5727ea44260cfe33c09e284f2516e
QuintsGuy <knotttristan@gmail.com>
2025-05-06 18:39:20 -0400

    Finished creating and designing terms and conditions page

---
+++ src/app/components/public/terms-and-conditions/terms-and-conditions.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-and-conditions',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './terms-and-conditions.component.html',
+  styleUrl: './terms-and-conditions.component.css'
+})
+export class TermsAndConditionsComponent {
+
+}
