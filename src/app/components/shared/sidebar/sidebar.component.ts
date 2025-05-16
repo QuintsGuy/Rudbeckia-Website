09c47a0752996aa2b6a1409f24e79835f0dc7601
Tristan Knott <122324806+QuintsGuy@users.noreply.github.com>
2025-05-16 11:25:42 -0400

    Update sidebar.component.ts

--- src/app/components/shared/sidebar/sidebar.component.ts
+++ src/app/components/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AuthService } from '../../../services/auth.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,10 +11,14 @@ import { AuthService } from '../../../services/auth.service';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
   unreadCount = 0;
   
   constructor(private authService: AuthService) {}
+
+  ngAfterViewInit(): void {
+    initFlowbite();
+  }
   
   Logout() {
     this.authService.logout();
