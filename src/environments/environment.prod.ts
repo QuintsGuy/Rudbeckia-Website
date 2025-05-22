af613057ea47774fb9dd99e6a667b8a50acd334f
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-05-22 17:19:56 -0400

    Fixing environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ declare const NG_ENV: any;
 
 export const environment = {
     production: true,
+    bypassAuth: false,
     SUPABASE_URL: NG_ENV.SUPABASE_URL,
     SUPABASE_KEY: NG_ENV.SUPABASE_KEY,
-    BYPASS_AUTH: NG_ENV.BYPASS_AUTH === 'true'
 };
