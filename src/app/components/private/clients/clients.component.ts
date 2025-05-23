c7d8cfe9291464d9b3e6e90d737630fe38cc0492
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-05-22 20:48:16 -0400

    Setup clients and coordinator crud pages in admin dashboard

--- src/app/components/private/clients/clients.component.ts
+++ src/app/components/private/clients/clients.component.ts
@@ -1,11 +1,83 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { SupabaseService } from '../../../services/supabase.service';
+import { Router } from '@angular/router';
+import { ToastService } from '../../../services/toast.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-clients',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './clients.component.html',
   styleUrl: './clients.component.css'
 })
 export class ClientsComponent {
+  clients: any[] = [];
+  targetClient: any = null;
+  events: any[] = [];
+  editableEvents: any[] = [];
+  editingEvents: boolean = false;
+  loading: boolean = true;
+  actionLoading: boolean = false;
 
+  user: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalRecords: number = 0;
+
+  constructor(
+    private supabase: SupabaseService,
+    private router: Router,
+    private toast: ToastService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const session = await this.supabase.getClient().auth.getSession();
+    this.user = session.data.session?.user;
+
+    if(!this.user) {
+      this.router.navigate(['/']);
+      return;
+    }
+
+    await this.fetchClients();
+  }
+
+  async fetchClients() {
+    this.loading = true;
+    
+    const from = (this.currentPage - 1) * this.pageSize;
+    const to = from + this.pageSize - 1;
+
+    const { data: clientsData, count, error: fetchError } = await this.supabase.getClient()
+      .from('clients')
+      .select(`*, event:events (*)`)
+      .order('created_at', {ascending: false})
+      .range(from, to);
+    
+    console.log(clientsData);
+    
+    if (fetchError) {
+      console.error('Failed to load clients: ', fetchError.message);
+      this.toast.showToast('Failed to load clients', 'error');
+    } else {
+      this.clients = clientsData?.map(client => ({
+        ...client,
+        isExpanded: false,
+      })) || [];
+      this.totalRecords = count || 0;
+    }
+    this.loading = false;
+  }
+
+  toggleExpand(client: any) {
+    client.expanded = !client.expanded;
+
+    if (client.expanded && client.events?.length) {
+      client.events = [...client.event].sort((a, b) => {
+        return new Date(a.due_date).getTime() - new Date(b.due_date).getTime(); // ascending
+      });
+    }
+  }
 }
--- src/app/components/private/coordinators/coordinators.component.ts
+++ src/app/components/private/coordinators/coordinators.component.ts
@@ -1,11 +1,82 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SupabaseService } from '../../../services/supabase.service';
+import { Router } from '@angular/router';
+import { ToastService } from '../../../services/toast.service';
 
 @Component({
   selector: 'app-coordinators',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './coordinators.component.html',
   styleUrl: './coordinators.component.css'
 })
 export class CoordinatorsComponent {
+  coordinators: any[] = [];
+  targetCoordinator: any = null;
+  events: any[] = [];
+  editableEvents: any[] = [];
+  editingEvents: boolean = false;
+  loading: boolean = true;
+  actionLoading: boolean = false;
 
+  user: any;
+  currentPage: number = 1;
+  pageSize: number = 10;
+  totalRecords: number = 0;
+
+  constructor(
+    private supabase: SupabaseService,
+    private router: Router,
+    private toast: ToastService
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const session = await this.supabase.getClient().auth.getSession();
+    this.user = session.data.session?.user;
+
+    if(!this.user) {
+      this.router.navigate(['/']);
+      return;
+    }
+
+    await this.fetchCoordinators();
+  }
+
+  async fetchCoordinators() {
+    this.loading = true;
+    
+    const from = (this.currentPage - 1) * this.pageSize;
+    const to = from + this.pageSize - 1;
+
+    const { data: coordinatorsData, count, error: fetchError } = await this.supabase.getClient()
+      .from('coordinators')
+      .select(`*, event:events (*)`)
+      .order('created_at', {ascending: false})
+      .range(from, to);
+    
+    console.log(coordinatorsData);
+    
+    if (fetchError) {
+      console.error('Failed to load coordinators: ', fetchError.message);
+      this.toast.showToast('Failed to load coordinators', 'error');
+    } else {
+      this.coordinators = coordinatorsData?.map(coordinator => ({
+        ...coordinator,
+        isExpanded: false,
+      })) || [];
+      this.totalRecords = count || 0;
+    }
+    this.loading = false;
+  }
+
+  toggleExpand(coordinator: any) {
+    coordinator.expanded = !coordinator.expanded;
+
+    if (coordinator.expanded && coordinator.events?.length) {
+      coordinator.events = [...coordinator.event].sort((a, b) => {
+        return new Date(a.due_date).getTime() - new Date(b.due_date).getTime(); // ascending
+      });
+    }
+  }
 }
