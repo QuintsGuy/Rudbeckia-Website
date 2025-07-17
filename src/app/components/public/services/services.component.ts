8ac66abda9f479cf835c2b0d6f844555a44d5a85
QuintsGuy <122324806+QuintsGuy@users.noreply.github.com>
2025-07-16 20:09:11 -0400

    Created footer and implemented services page logic

--- src/app/components/public/services/services.component.ts
+++ src/app/components/public/services/services.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 
 @Component({
   selector: 'app-services',
@@ -9,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './services.component.css'
 })
 export class ServicesComponent {
+  tabs = ['Weddings', 'Parties', 'Graduations', 'Funerals', 'Other'];
+  selectedTab = 'Weddings';
 
+  selectTab(tab: string) {
+    this.selectedTab = tab
+  }
 }
+
+
--- src/app/layouts/public-layout/public-layout.component.ts
+++ src/app/layouts/public-layout/public-layout.component.ts
@@ -2,11 +2,12 @@ import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../components/shared/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './public-layout.component.html',
   styleUrl: './public-layout.component.css'
 })
